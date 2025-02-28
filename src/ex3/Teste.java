3bba1fa1330f266eb597a749bbad43a5d2616a55
NandaMariaa <nandamaria182@gmail.com>
2025-02-28 20:28:58 -0300

    biblioteca

---
+++ src/ex3/Teste.java
@@ -0,0 +1,19 @@
+package ex3;
+
+import java.util.ArrayList;
+
+public class Teste {
+
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("Elemento A");
+        arrayList.add("Elemento B");
+        arrayList.add("Elemento C");
+
+        System.out.println(arrayList);
+        
+    }
+
+}
