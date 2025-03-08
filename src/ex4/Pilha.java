7643989a4eefeb29739fb5b4c22847e7caac3152
NandaMariaa <nandamaria182@gmail.com>
2025-03-07 21:03:57 -0300

    ex4

---
+++ src/ex4/Pilha.java
@@ -0,0 +1,22 @@
+package ex4;
+
+public class Pilha <T> extends EstruturaEstatica <T> {
+
+    public Pilha () {
+
+        super();
+
+    }
+    
+    public Pilha(int capacidade) {
+
+        super(capacidade);
+
+    }
+
+    public void empilha (T elemento) {
+
+        super.adiciona(elemento);
+        
+    }
+}
