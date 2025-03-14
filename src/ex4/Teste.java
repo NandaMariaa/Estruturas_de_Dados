4a5a035f50b59b0ceaf73054a9bd868e5d5f8638
NandaMariaa <nandamaria182@gmail.com>
2025-03-14 20:24:11 -0300

    fila

--- src/ex4/Teste.java
+++ src/ex4/Teste.java
@@ -4,28 +4,43 @@ public class Teste {
 
     public static void main(String[] args) {
 
-         Pilha <Integer> pilha = new Pilha <Integer> ();
+        //Pilha <Integer> pilha = new Pilha <Integer> ();
 
-            //for (int i = 1; i <= 10; i++) {
-            //pilha.empilha(i);
+        //for (int i = 1; i <= 10; i++) {
+        //pilha.empilha(i);
 
-            System.out.println(pilha.topo());
+        //System.out.println(pilha.topo());
 
-            System.out.println(pilha);
+        //System.out.println(pilha);
 
-            pilha.empilha(1);
-            pilha.empilha(2);
-            pilha.empilha(3);
+        //pilha.empilha(1);
+        //pilha.empilha(2);
+        //pilha.empilha(3);
 
-            System.out.println(pilha.topo());
+        //System.out.println(pilha.topo());
 
-            System.out.println(pilha);
+        //System.out.println(pilha);
 
-            pilha.desempilha();
-            System.out.println(pilha);
+        //pilha.desempilha();
+        //System.out.println(pilha);
 
-           
+        //aula 5
 
+        Fila<Integer> fila = new Fila<Integer>();
+        fila.enfileira(1);
+        fila.enfileira(2);
+        fila.enfileira(3);
+        fila.enfileira(4);
+        fila.enfileira(5);
+
+        System.out.println(fila.toString());
+
+        System.out.println(fila.espiar());
+        if(fila.espiar() == null) {
+            System.out.println("ninguem na fila");
+        } else {
+            System.out.println("primeira posição: " + fila.espiar());
         }
+    }
 }
     
